59046fa6ba50337d1d0c40552d357e478346a593
MenekseYuncu <baioretto66@gmail.com>
2024-01-24 14:30:58 +0300

    Initial commit

---
+++ src/main/java/org/violet/restaurantmanagement/RestaurantManagementApplication.java
@@ -0,0 +1,13 @@
+package org.violet.restaurantmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/violet/restaurantmanagement/RestaurantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.violet.restaurantmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
