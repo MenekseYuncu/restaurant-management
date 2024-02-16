391ef3233cb68f23bf7894f7d74f53dc312dfb05
MenekseYuncu <baioretto66@gmail.com>
2024-02-16 23:50:33 +0300

    initialising db table

--- src/main/resources/db/migration/V1__create_database.sql
+++ src/main/resources/db/migration/V1__create_database.sql
@@ -1,3 +1,14 @@
+create table if not exists category
+(
+    id         bigserial
+        constraint pk__category__id primary key,
+    name       varchar(300) not null,
+    status     varchar(20)  not null
+        constraint c__category__status check (status in ('ACTIVE', 'INACTIVE')),
+    created_at timestamp(0)    not null,
+    updated_at timestamp(0)
+);
+
 create table if not exists product
 (
     id          varchar(36)
@@ -11,19 +22,8 @@ create table if not exists product
         constraint p__product__status check (status in ('ACTIVE', 'INACTIVE')),
     extent      integer        not null,
     extent_type varchar(5)     not null check (extent_type IN ('ML', 'GR')),
-    created_at  timestamp      not null,
-    updated_at  timestamp
-);
-
-create table if not exists category
-(
-    id         bigserial
-        constraint pk__category__id primary key,
-    name       varchar(300) not null,
-    status     varchar(20)  not null
-        constraint c__category__status check (status in ('ACTIVE', 'INACTIVE')),
-    created_at timestamp    not null,
-    updated_at timestamp
+    created_at  timestamp(0)      not null,
+    updated_at  timestamp(0)
 );
 
 create table if not exists dining_table
@@ -32,8 +32,9 @@ create table if not exists dining_table
         constraint pk__dining_table__id primary key,
     merge_id            varchar(36) not null,
     dining_table_status pg_enum     not null,
-    created_at          timestamp   not null,
-    updated_at          timestamp
+    size                int         not null,
+    created_at          timestamp(0)   not null,
+    updated_at          timestamp(0)
 );
 
 create table if not exists orders
@@ -44,7 +45,7 @@ create table if not exists orders
     order_status          pg_enum        not null,
     price                 numeric(50, 8) not null,
     created_at            timestamp(3)   not null,
-    updated_at            timestamp
+    updated_at            timestamp(3)
 );
 
 create table if not exists order_item
@@ -58,17 +59,17 @@ create table if not exists order_item
     price             numeric(50, 8) not null,
     order_item_status pg_enum        not null,
     created_at        timestamp(3)   not null,
-    updated_at        timestamp
+    updated_at        timestamp(3)
 );
 
 create table if not exists parameter
 (
-    id          bigserial
+    id         bigserial
         constraint pk__parameter__id primary key,
-    name        varchar(50) not null,
-    description char(3),
-    created_at  timestamp   not null,
-    updated_at  timestamp
+    name       varchar(50) not null,
+    currency   char(3),
+    created_at timestamp(0)   not null,
+    updated_at timestamp(0)
 );
 
 create type dining_table_status as enum ('VACANT', 'OCCUPİED', 'TAKING_ORDERS', 'RESERVED');
@@ -76,15 +77,16 @@ create type order_status as enum ('OPEN', 'IN_PROGRESS', 'COMPLETED', 'CANCELED'
 create type order_item_status as enum ('PREPARİNG', 'READY', 'DELİVERED', 'CANCELED');
 
 
-INSERT INTO product (id, category_id, name, ingredient, price, status, extent, extent_type, created_at)
-VALUES ('0fe5d76a-99b6-11ea-bb37-0242ac130002', '1s2a376a-99b6-11ea-bb37-0242ac161589', 'pname', 'ingredient',
-        20.3, 'ACTIVE', 200, 'ML', CURRENT_TIMESTAMP),
-       ('e94186d1-8c52-4c57-b7c4-a5d5cfe2977c', '63c8c4b7-59af-476c-8295-30e4881132c1', 'pname2', 'ingredient',
-        20.3, 'ACTIVE', 200, 'GR', CURRENT_TIMESTAMP);
+insert into category(name, status, created_at)
+values ('category 1', 'ACTIVE', CURRENT_TIMESTAMP),
+       ('category 2', 'ACTIVE', CURRENT_TIMESTAMP);
 
-INSERT INTO category(id, name, status, created_at)
-VALUES ('1s2a376a-99b6-11ea-bb37-0242ac161589', 'cname', 'ACTIVE', CURRENT_TIMESTAMP),
-       ('63c8c4b7-59af-476c-8295-30e4881132c1', 'cname2', 'ACTIVE', CURRENT_TIMESTAMP);
+insert into product (id, category_id, name, ingredient, price, status, extent, extent_type, created_at)
+values ('0fe5d76a-99b6-11ea-bb37-0242ac130002', 1, 'product 1', 'ingredient', 20.3, 'ACTIVE', 200, 'ML',
+        CURRENT_TIMESTAMP),
+       ('e94186d1-8c52-4c57-b7c4-a5d5cfe2977c', 2, 'product 2', 'ingredient', 20.3, 'ACTIVE', 200, 'GR',
+        CURRENT_TIMESTAMP);
 
-INSERT INTO parameter(id, name, description, created_at)
-VALUES ('7c8afe20-7f49-4c1e-b044-81d975d940f5', 'name', 'TRY', CURRENT_TIMESTAMP);
+insert into parameter(name, currency, created_at)
+values ('Turkish money', 'TRY', CURRENT_TIMESTAMP);
+
