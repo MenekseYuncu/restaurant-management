9a954267f14db1a44db4f3b515a9d986312e068f
MenekseYuncu <baioretto66@gmail.com>
2024-02-17 17:25:26 +0300

    initialising db table fixed

--- src/main/resources/db/migration/V1__init_database.sql
+++ src/main/resources/db/migration/V1__init_database.sql
@@ -1,35 +1,36 @@
-create table if not exists category
+create table if not exists rm_category
 (
     id         bigserial
-        constraint pk__category__id primary key,
+        constraint pk__rm_category__id primary key,
     name       varchar(300) not null,
     status     varchar(20)  not null
-        constraint c__category__status check (status in ('ACTIVE', 'INACTIVE')),
+        constraint c__rm_category__status check (status in ('ACTIVE', 'INACTIVE')),
     created_at timestamp(0)    not null,
     updated_at timestamp(0)
 );
 
-create table if not exists product
+create table if not exists rm_product
 (
     id          varchar(36)
-        constraint pk__product__id primary key,
+        constraint pk__rm_product__id primary key,
     category_id bigint
-        constraint fk__product__category__id references category (id),
+        constraint fk__rm_product__category__id references category (id),
     name        varchar(300)   not null,
     ingredient  varchar(2048)  not null,
     price       numeric(50, 8) not null,
     status      varchar(20)    not null
-        constraint p__product__status check (status in ('ACTIVE', 'INACTIVE')),
+        constraint c__rm_product__status check (status in ('ACTIVE', 'INACTIVE')),
     extent      integer        not null,
-    extent_type varchar(5)     not null check (extent_type IN ('ML', 'GR')),
+    extent_type varchar(5)     not null
+        constraint c__rm_product__extent_type check (extent_type IN ('ML', 'GR')),
     created_at  timestamp(0)      not null,
     updated_at  timestamp(0)
 );
 
-create table if not exists dining_table
+create table if not exists rm_dining_table
 (
     id                  bigserial
-        constraint pk__dining_table__id primary key,
+        constraint pk__rm_dining_table__id primary key,
     merge_id            varchar(36) not null,
     dining_table_status pg_enum     not null,
     size                int         not null,
@@ -37,10 +38,10 @@ create table if not exists dining_table
     updated_at          timestamp(0)
 );
 
-create table if not exists orders
+create table if not exists rm_order
 (
     id                    varchar(36)
-        constraint pk__orders__id primary key,
+        constraint pk__rm_order__id primary key,
     dining_table_merge_id varchar(36)    not null,
     order_status          pg_enum        not null,
     price                 numeric(50, 8) not null,
@@ -48,45 +49,46 @@ create table if not exists orders
     updated_at            timestamp(3)
 );
 
-create table if not exists order_item
+create table if not exists rm_order_item
 (
     id                varchar(36)
-        constraint pk__order__id primary key,
+        constraint pk__rm_order_item__id primary key,
     order_id          varchar(36)
-        constraint fk__order_item__id references orders (id),
+        constraint fk__rm_order_item__id references orders (id),
     product_id        varchar(36)
-        constraint fk__product__id references product (id),
+        constraint fk__rm_order_item__id references product (id),
     price             numeric(50, 8) not null,
     order_item_status pg_enum        not null,
     created_at        timestamp(3)   not null,
     updated_at        timestamp(3)
 );
 
-create table if not exists parameter
+create table if not exists rm_parameter
 (
     id         bigserial
-        constraint pk__parameter__id primary key,
-    name       varchar(50) not null,
-    currency   char(3),
+        constraint pk__rm_parameter__id primary key,
+    name       varchar(200) not null
+ constraint u__rm_parameter_name unique,
+    definition   char(3),
     created_at timestamp(0)   not null,
     updated_at timestamp(0)
 );
 
-create type dining_table_status as enum ('VACANT', 'OCCUPİED', 'TAKING_ORDERS', 'RESERVED');
+create type dining_table_status as enum ('VACANT', 'OCCUPIED', 'TAKING_ORDERS', 'RESERVED');
 create type order_status as enum ('OPEN', 'IN_PROGRESS', 'COMPLETED', 'CANCELED');
-create type order_item_status as enum ('PREPARİNG', 'READY', 'DELİVERED', 'CANCELED');
+create type order_item_status as enum ('PREPARING', 'READY', 'DELIVERED', 'CANCELED');
 
 
 insert into category(name, status, created_at)
-values ('category 1', 'ACTIVE', CURRENT_TIMESTAMP),
-       ('category 2', 'ACTIVE', CURRENT_TIMESTAMP);
+values ('category 1', 'ACTIVE', current_timestamp),
+       ('category 2', 'ACTIVE', current_timestamp);
 
 insert into product (id, category_id, name, ingredient, price, status, extent, extent_type, created_at)
 values ('0fe5d76a-99b6-11ea-bb37-0242ac130002', 1, 'product 1', 'ingredient', 20.3, 'ACTIVE', 200, 'ML',
-        CURRENT_TIMESTAMP),
+        current_timestamp),
        ('e94186d1-8c52-4c57-b7c4-a5d5cfe2977c', 2, 'product 2', 'ingredient', 20.3, 'ACTIVE', 200, 'GR',
-        CURRENT_TIMESTAMP);
+        current_timestamp);
 
 insert into parameter(name, currency, created_at)
-values ('Turkish money', 'TRY', CURRENT_TIMESTAMP);
+values ('CURRENCY', 'TRY', current_timestamp);
 
