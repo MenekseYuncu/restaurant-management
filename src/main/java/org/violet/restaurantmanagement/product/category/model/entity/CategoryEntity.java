c4ea329e8f59cb8bd8ff856fdb23459720848fdc
MenekseYuncu <baioretto66@gmail.com>
2024-02-18 21:29:23 +0300

    category entity and domain created

--- src/main/java/org/violet/restaurantmanagement/product/category/model/entity/CategoryEntity.java
+++ src/main/java/org/violet/restaurantmanagement/product/category/model/entity/CategoryEntity.java
@@ -1,4 +1,35 @@
 package org.violet.restaurantmanagement.product.category.model.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.violet.restaurantmanagement.product.category.model.enums.CategoryStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "rm_category")
 public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "status")
+    private CategoryStatus status;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
--- src/main/java/org/violet/restaurantmanagement/product/category/service/domain/Category.java
+++ src/main/java/org/violet/restaurantmanagement/product/category/service/domain/Category.java
@@ -1,4 +1,16 @@
 package org.violet.restaurantmanagement.product.category.service.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.violet.restaurantmanagement.product.category.model.enums.CategoryStatus;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Category {
+
+    private Long id;
+    private String name;
+    private CategoryStatus status;
 }
