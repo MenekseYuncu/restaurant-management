77e089c14195f352f2efebdfd718af2bb35dabc3
MenekseYuncu <baioretto66@gmail.com>
2024-03-11 21:48:54 +0300

    GlobalExceptionHandler added and CategoryNotFoundException modified

---
+++ src/main/java/org/violet/restaurantmanagement/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package org.violet.restaurantmanagement.common.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.violet.restaurantmanagement.product.exceptions.CategoryNotFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ResponseEntity<Object> categoryNotFoundExceptionHandler(CategoryNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
--- src/main/java/org/violet/restaurantmanagement/product/exceptions/CategoryNotFoundException.java
+++ src/main/java/org/violet/restaurantmanagement/product/exceptions/CategoryNotFoundException.java
@@ -1,5 +1,9 @@
 package org.violet.restaurantmanagement.product.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CategoryNotFoundException extends RuntimeException{
 
     public CategoryNotFoundException(String message) {
