7089ad57ee292bcf5a4745548eb747f1a1ecd052
MenekseYuncu <baioretto66@gmail.com>
2024-04-18 23:35:28 +0300

    added statuses and name restriction

--- src/main/java/org/violet/restaurantmanagement/product/controller/util/CategoryFilter.java
+++ src/main/java/org/violet/restaurantmanagement/product/controller/util/CategoryFilter.java
@@ -1,5 +1,6 @@
 package org.violet.restaurantmanagement.product.controller.util;
 
+import jakarta.persistence.criteria.Predicate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,9 @@ import lombok.Setter;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
 import org.violet.restaurantmanagement.common.pegable.Filtering;
+import org.violet.restaurantmanagement.product.model.enums.CategoryStatus;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -16,12 +20,21 @@ public class CategoryFilter implements Filtering {
 
     private String name;
 
+    private Set<CategoryStatus> statuses;
+
     public <C> Specification<C> toSpecification() {
         return (root, query, criteriaBuilder) -> {
+
+            Predicate predicate = criteriaBuilder.conjunction();
+
             if (StringUtils.hasText(name)) {
-                return criteriaBuilder.equal(root.get("name"), name);
+                predicate = criteriaBuilder.and(predicate, criteriaBuilder.like
+                        (criteriaBuilder.lower(root.get("name")), "%" + name.toLowerCase() + "%"));
             }
-            return null;
+            if (statuses != null && !statuses.isEmpty()) {
+                predicate = criteriaBuilder.and(predicate, root.get("status").in(statuses));
+            }
+            return predicate;
         };
     }
 }
