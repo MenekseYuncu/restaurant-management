896cf3f4baaf6650695ddf1435c67a746d497957
MenekseYuncu <baioretto66@gmail.com>
2024-04-19 00:00:13 +0300

    repository modified

--- src/main/java/org/violet/restaurantmanagement/product/repository/CategoryRepository.java
+++ src/main/java/org/violet/restaurantmanagement/product/repository/CategoryRepository.java
@@ -1,16 +1,12 @@
 package org.violet.restaurantmanagement.product.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import org.violet.restaurantmanagement.product.repository.entity.CategoryEntity;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> , JpaSpecificationExecutor<CategoryEntity> {
 
-    boolean existsByName(String name);
-
-    Page<CategoryEntity> findAll(Specification<CategoryEntity> specification, Pageable pageable);
+    boolean findByName(String name);
 }
