d4ba6dcc821603f2427aee502c43f5659be0c8c6
MenekseYuncu <baioretto66@gmail.com>
2024-05-03 23:34:11 +0300

    BaseTest added

---
+++ src/test/java/org/violet/restaurantmanagement/product/BaseTest.java
@@ -0,0 +1,22 @@
+package org.violet.restaurantmanagement.product;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
+public abstract class BaseTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+}
