4cd57b104f0352f4bfe8b648b4ee9bb555a25066
MenekseYuncu <baioretto66@gmail.com>
2024-05-07 23:08:38 +0300

    pagination builder added

--- src/main/java/org/violet/restaurantmanagement/common/pegable/Pagination.java
+++ src/main/java/org/violet/restaurantmanagement/common/pegable/Pagination.java
@@ -3,10 +3,12 @@ package org.violet.restaurantmanagement.common.pegable;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Range;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class Pagination {
 
