e8920c1628593bd3c76698e4f2512086a64950b3
MenekseYuncu <baioretto66@gmail.com>
2024-05-07 23:10:55 +0300

    RmTest and test container implemented

--- src/test/java/org/violet/restaurantmanagement/product/controller/CategoryTest.java
+++ src/test/java/org/violet/restaurantmanagement/product/controller/CategoryTest.java
@@ -1,26 +1,19 @@
 package org.violet.restaurantmanagement.product.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.violet.restaurantmanagement.common.pegable.PageContent;
-import org.violet.restaurantmanagement.common.pegable.Pagination;
 import org.violet.restaurantmanagement.product.model.enums.CategoryStatus;
 import org.violet.restaurantmanagement.product.service.CategoryService;
 import org.violet.restaurantmanagement.product.service.command.CategoryCreateCommand;
-import org.violet.restaurantmanagement.product.service.command.CategoryListCommand;
 import org.violet.restaurantmanagement.product.service.command.CategoryUpdateCommand;
 import org.violet.restaurantmanagement.product.service.domain.Category;
 
@@ -30,46 +23,21 @@ import java.time.LocalDateTime;
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class CategoryTest {
+class CategoryTest extends RMTest implements RestaurantManagementTestContainer {
 
     @LocalServerPort
     private int port;
 
     private String BASE_URL = "http://localhost";
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @MockBean
     private CategoryService categoryService;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @BeforeEach
     public void setUp() {
         BASE_URL = BASE_URL.concat(":").concat(port + "").concat("/api/v1/category");
     }
 
-
-    @Test
-    void testGetAllCategories() throws Exception {
-        PageContent<Category> pageContent = new PageContent<>();
-        CategoryListCommand categoryListCommand = CategoryListCommand.builder()
-                .pagination(Pagination.builder().pageSize(10).pageNumber(1).build())
-                .filter(null)
-                .sorting(null)
-                .build();
-
-        Mockito.when(categoryService.getAllCategories(ArgumentMatchers.any(CategoryListCommand.class)))
-                .thenReturn(pageContent);
-
-        mockMvc.perform(MockMvcRequestBuilders.post(BASE_URL + "/categories")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(categoryListCommand)))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
-    }
 
     @Test
     void testGetCategoryById() throws Exception {
