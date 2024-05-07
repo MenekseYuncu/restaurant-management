5c2e01e2f9f1a7fb32d783e18379a77c12941506
MenekseYuncu <baioretto66@gmail.com>
2024-05-07 23:09:32 +0300

    Test Container added

---
+++ src/test/java/org/violet/restaurantmanagement/product/controller/RestaurantManagementTestContainer.java
@@ -0,0 +1,26 @@
+package org.violet.restaurantmanagement.product.controller;
+
+import org.junit.jupiter.api.AfterAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+interface RestaurantManagementTestContainer {
+    PostgreSQLContainer<?> POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:latest")
+            .withUsername("restaurant_user")
+            .withPassword("restaurant_password")
+            .withDatabaseName("restaurant_management");
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        POSTGRESQL_CONTAINER.start();
+        dynamicPropertyRegistry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
+        dynamicPropertyRegistry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
+    }
+
+    @AfterAll
+    static void stopContainer() {
+        POSTGRESQL_CONTAINER.stop();
+    }
+}
