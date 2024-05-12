62c877767d17b39b63aceb9f7b15d975ee4cd846
MenekseYuncu <baioretto66@gmail.com>
2024-05-13 02:18:49 +0300

    toSpecification modified

--- src/main/java/org/violet/restaurantmanagement/product/service/command/CategoryListCommand.java
+++ src/main/java/org/violet/restaurantmanagement/product/service/command/CategoryListCommand.java
@@ -5,12 +5,16 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.violet.restaurantmanagement.common.model.Filtering;
 import org.violet.restaurantmanagement.common.model.mapper.RmaSpecification;
 import org.violet.restaurantmanagement.common.service.command.RmaPaginationCommand;
 import org.violet.restaurantmanagement.product.model.enums.CategoryStatus;
 
 import java.util.Set;
+
+import static java.lang.StringTemplate.STR;
 
 @Getter
 @SuperBuilder
@@ -38,20 +42,18 @@ public class CategoryListCommand extends RmaPaginationCommand implements RmaSpec
 
         Specification<C> specification = Specification.where(null);
 
-        if (this.filter.getName() != null) {
-            Specification<C> tempSpecification = ((root, query, criteriaBuilder) ->
-                    criteriaBuilder.like(root.get("name"), this.filter.getName()));
-
-            specification.and(tempSpecification);
+        if (StringUtils.hasText(this.filter.getName())) {
+            Specification<C> tempSpecification = (root, _, criteriaBuilder) ->
+                    criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), STR."%\{filter.getName().toLowerCase()}%");
+            specification = specification.and(tempSpecification);
         }
 
-        if (this.filter.getStatuses() != null) {
-            Specification<C> tempSpecification = ((root, query, criteriaBuilder) ->
-                    criteriaBuilder.equal(root.get("statuses"), this.filter.getStatuses()));
-
-            specification.and(tempSpecification);
+        if (!CollectionUtils.isEmpty(this.filter.getStatuses())) {
+            Specification<C> statusSpecification = (root, _, _) ->
+                    root.get("status").in(this.filter.getStatuses());
+            specification = specification.and(statusSpecification);
         }
 
-        return Specification.allOf();
+        return specification;
     }
 }
