baea388296b5c40ae81c7ea9b8d0354a3c8e7c6e
MenekseYuncu <baioretto66@gmail.com>
2024-05-20 12:45:27 +0300

    category tests are modified

--- src/test/java/org/violet/restaurantmanagement/category/controller/CategoryEndToEndTest.java
+++ src/test/java/org/violet/restaurantmanagement/category/controller/CategoryEndToEndTest.java
@@ -1,109 +1,123 @@
 package org.violet.restaurantmanagement.category.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Sort;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.violet.restaurantmanagement.RmaEndToEndTest;
+import org.violet.restaurantmanagement.RmaTestContainer;
+import org.violet.restaurantmanagement.category.controller.request.CategoryCreateRequest;
 import org.violet.restaurantmanagement.category.controller.request.CategoryListRequest;
+import org.violet.restaurantmanagement.category.controller.request.CategoryUpdateRequest;
+import org.violet.restaurantmanagement.category.exceptions.CategoryNotFoundException;
 import org.violet.restaurantmanagement.category.model.enums.CategoryStatus;
-import org.violet.restaurantmanagement.category.service.CategoryService;
-import org.violet.restaurantmanagement.category.service.command.CategoryCreateCommand;
-import org.violet.restaurantmanagement.category.service.command.CategoryListCommand;
-import org.violet.restaurantmanagement.category.service.command.CategoryUpdateCommand;
-import org.violet.restaurantmanagement.category.service.domain.Category;
-import org.violet.restaurantmanagement.common.model.Pagination;
-import org.violet.restaurantmanagement.common.model.RmaPage;
-import org.violet.restaurantmanagement.common.model.Sorting;
-import org.violet.restaurantmanagement.util.RmaEndToEndTest;
-import org.violet.restaurantmanagement.util.RmaTestContainer;
-
-import java.time.LocalDateTime;
+import org.violet.restaurantmanagement.category.repository.CategoryRepository;
+import org.violet.restaurantmanagement.category.repository.entity.CategoryEntity;
+import org.violet.restaurantmanagement.common.model.PaginationBuilder;
+import org.violet.restaurantmanagement.common.model.SortingBuilder;
 
 class CategoryEndToEndTest extends RmaEndToEndTest implements RmaTestContainer {
 
-    @MockBean
-    private CategoryService categoryService;
-
+    @Autowired
+    private CategoryRepository categoryRepository;
     private final static String BASE_URL = "/api/v1/category";
 
     @Test
-    void testGetAllCategories() throws Exception {
-        CategoryListCommand.CategoryFilter filter = CategoryListCommand.CategoryFilter.builder().name("Test").build();
-        CategoryListRequest categoryListRequest = CategoryListRequest.builder()
-                .pagination(Pagination.builder().pageNumber(2).pageSize(1).build())
-                .sorting(Sorting.builder().direction(Sort.Direction.ASC).property("name").build())
-                .filter(CategoryListRequest.CategoryFilter.builder().name("Category").build())
+    void whenCategoryListRequestExist_thenReturnCategories() throws Exception {
+        CategoryListRequest mockCategoryListRequest = CategoryListRequest.builder()
+                .pagination(PaginationBuilder.builder()
+                        .pageNumber(1)
+                        .pageSize(1)
+                        .build()
+                )
+                .sorting(SortingBuilder.builder()
+                        .asc()
+                        .property("name")
+                        .build()
+                )
+                .filter(CategoryListRequest.CategoryFilter.builder()
+                        .name("Category")
+                        .build()
+                )
                 .build();
-
-        RmaPage<Category> rmaPage = RmaPage.<Category>builder()
-                .pageNumber(1)
-                .pageSize(2)
-                .totalPageCount(1)
-                .totalElementCount(3L)
-                .sortedBy(categoryListRequest.getSorting())
-                .filteredBy(filter)
-                .build();
-
-        Mockito.when(categoryService.getAllCategories(
-                Mockito.any(CategoryListCommand.class))
-        ).thenReturn(rmaPage);
 
         mockMvc.perform(MockMvcRequestBuilders.post(BASE_URL + "/categories")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(categoryListRequest)))
+                        .content(objectMapper.writeValueAsString(mockCategoryListRequest)))
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.isSuccess").value(true))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.response").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.pageNumber")
+                        .value(mockCategoryListRequest.getPagination().getPageNumber()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.pageSize")
+                        .value(mockCategoryListRequest.getPagination().getPageSize()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.httpStatus").value("OK"));
+    }
+    @Test
+    void testGetCategoryById() throws Exception {
+        Long categoryId = 1L;
+
+        CategoryEntity categoryEntity = categoryRepository.findById(categoryId)
+                .orElseThrow(CategoryNotFoundException::new);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/{id}", categoryId))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.id").value(categoryEntity.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.name").value(categoryEntity.getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.status").value(categoryEntity.getStatus()
+                        .toString()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.httpStatus").value("OK"));
     }
 
     @Test
-    void testGetCategoryById() throws Exception {
-        Category category = Category.builder()
-                .id(1L)
-                .name("Test")
-                .status(CategoryStatus.ACTIVE)
-                .createdAt(LocalDateTime.now()).build();
-
-        Mockito.when(categoryService.getCategoryById(1L)).thenReturn(category);
-
-        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/{id}", 1L))
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-    }
-
-    @Test
-    void testCreateCategory() throws Exception {
-        CategoryCreateCommand command = new CategoryCreateCommand("Test", CategoryStatus.ACTIVE);
-
-        Mockito.doNothing().when(categoryService).createCategory(Mockito.any(CategoryCreateCommand.class));
+    void whenCategoryCreateRequestExist_thenReturnSuccess() throws Exception {
+        CategoryCreateRequest mockCategoryCreateRequest = new CategoryCreateRequest(
+                "Category",
+                CategoryStatus.ACTIVE
+        );
 
         mockMvc.perform(MockMvcRequestBuilders.post(BASE_URL)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(command)))
+                        .content(objectMapper.writeValueAsString(mockCategoryCreateRequest)))
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
+
+        Assertions.assertTrue(categoryRepository.existsByName(mockCategoryCreateRequest.name()));
     }
 
     @Test
-    void testUpdateCategory() throws Exception {
-        CategoryUpdateCommand command = new CategoryUpdateCommand("Test", CategoryStatus.DELETED);
+    void whenCategoryIdExist_thenUpdateCategory() throws Exception {
+        Long categoryId = 1L;
 
-        Mockito.doNothing().when(categoryService).updateCategory(1L, command);
+        CategoryUpdateRequest mockCategoryUpdateRequest = new CategoryUpdateRequest(
+                "Test",
+                CategoryStatus.DELETED
+        );
 
-        mockMvc.perform(MockMvcRequestBuilders.put(BASE_URL + "/{id}", 1L)
+        mockMvc.perform(MockMvcRequestBuilders.put(BASE_URL + "/{id}", categoryId)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(command)))
+                        .content(objectMapper.writeValueAsString(mockCategoryUpdateRequest)))
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
-    void testDeleteCategory() throws Exception {
-        Mockito.doNothing().when(categoryService).deleteCategory(1L);
+    void whenCategoryIdExist_thenDeleteCategory() throws Exception {
+        Long categoryId = 1L;
 
-        mockMvc.perform(MockMvcRequestBuilders.delete(BASE_URL + "/{id}", 1L))
+        CategoryEntity categoryEntity = categoryRepository.findById(categoryId)
+                .orElseThrow(CategoryNotFoundException::new);
+
+        categoryEntity.setStatus(CategoryStatus.DELETED);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete(BASE_URL + "/{id}", categoryId))
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
--- src/test/java/org/violet/restaurantmanagement/category/service/impl/CategoryServiceImplTest.java
+++ src/test/java/org/violet/restaurantmanagement/category/service/impl/CategoryServiceImplTest.java
@@ -8,8 +8,9 @@ import org.mockito.Mockito;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
+import org.violet.restaurantmanagement.RmaServiceTest;
+import org.violet.restaurantmanagement.RmaTestContainer;
 import org.violet.restaurantmanagement.category.exceptions.CategoryAlreadyExistsException;
 import org.violet.restaurantmanagement.category.exceptions.CategoryNotFoundException;
 import org.violet.restaurantmanagement.category.model.enums.CategoryStatus;
@@ -22,11 +23,9 @@ import org.violet.restaurantmanagement.category.service.command.CategoryCreateCo
 import org.violet.restaurantmanagement.category.service.command.CategoryListCommand;
 import org.violet.restaurantmanagement.category.service.command.CategoryUpdateCommand;
 import org.violet.restaurantmanagement.category.service.domain.Category;
-import org.violet.restaurantmanagement.common.model.Pagination;
+import org.violet.restaurantmanagement.common.model.PaginationBuilder;
 import org.violet.restaurantmanagement.common.model.RmaPage;
-import org.violet.restaurantmanagement.common.model.Sorting;
-import org.violet.restaurantmanagement.util.RmaServiceTest;
-import org.violet.restaurantmanagement.util.RmaTestContainer;
+import org.violet.restaurantmanagement.common.model.SortingBuilder;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -54,13 +53,25 @@ class CategoryServiceImplTest extends RmaServiceTest implements RmaTestContainer
     void givenGetAllCategories_whenCategoryListWithoutFilterAndSorting_thenReturnCategories() {
         // Given
         List<CategoryEntity> categoryEntities = new ArrayList<>();
-        categoryEntities.add(new CategoryEntity(1L, "Category 1", CategoryStatus.ACTIVE));
-        categoryEntities.add(new CategoryEntity(2L, "Category 2", CategoryStatus.DELETED));
+        categoryEntities.add(new CategoryEntity(
+                1L,
+                "Category 1",
+                CategoryStatus.ACTIVE)
+        );
+        categoryEntities.add(new CategoryEntity(
+                2L,
+                "Category 2",
+                CategoryStatus.DELETED)
+        );
 
         Page<CategoryEntity> categoryEntityPage = new PageImpl<>(categoryEntities);
 
         CategoryListCommand givenCategoryListCommand = CategoryListCommand.builder()
-                .pagination(Pagination.builder().pageNumber(1).pageSize(5).build())
+                .pagination(PaginationBuilder.builder()
+                        .pageNumber(1)
+                        .pageSize(2)
+                        .build()
+                )
                 .build();
 
         // When
@@ -72,6 +83,7 @@ class CategoryServiceImplTest extends RmaServiceTest implements RmaTestContainer
         RmaPage<Category> result = categoryService.getAllCategories(givenCategoryListCommand);
 
         // Assertions
+        Assertions.assertEquals(result.getTotalElementCount(), categoryEntities.size());
         Assertions.assertEquals(categoryEntities.size(), result.getContent().size());
     }
 
@@ -79,16 +91,30 @@ class CategoryServiceImplTest extends RmaServiceTest implements RmaTestContainer
     void givenGetAllCategories_whenCategoryListWithoutSorting_thenReturnCategories() {
         // Given
         List<CategoryEntity> categoryEntities = new ArrayList<>();
-        categoryEntities.add(new CategoryEntity(1L, "Category 1", CategoryStatus.ACTIVE));
-        categoryEntities.add(new CategoryEntity(2L, "Category 2", CategoryStatus.DELETED));
+        categoryEntities.add(new CategoryEntity(
+                1L,
+                "Category 1",
+                CategoryStatus.ACTIVE)
+        );
+        categoryEntities.add(new CategoryEntity(
+                2L,
+                "Category 2",
+                CategoryStatus.DELETED)
+        );
 
         Page<CategoryEntity> categoryEntityPage = new PageImpl<>(categoryEntities);
 
         CategoryListCommand givenCategoryListCommand = CategoryListCommand.builder()
-                .pagination(Pagination.builder().pageNumber(1).pageSize(5).build())
+                .pagination(PaginationBuilder.builder()
+                        .pageNumber(1)
+                        .pageSize(2)
+                        .build()
+                )
                 .filter(CategoryListCommand.CategoryFilter.builder()
                         .name("Category")
-                        .statuses(Collections.singleton(CategoryStatus.ACTIVE)).build())
+                        .statuses(Collections.singleton(CategoryStatus.ACTIVE))
+                        .build()
+                )
                 .build();
 
         // When
@@ -114,44 +140,64 @@ class CategoryServiceImplTest extends RmaServiceTest implements RmaTestContainer
     @Test
     void givenGetAllCategories_whenCategoryListExist_thenReturnCategories() {
         // Given
-        List<CategoryEntity> categoryEntities = new ArrayList<>();
-        categoryEntities.add(new CategoryEntity(1L, "Category 1", CategoryStatus.ACTIVE));
-        categoryEntities.add(new CategoryEntity(2L, "Category 2", CategoryStatus.DELETED));
-        categoryEntities.add(new CategoryEntity(2L, "Category 3", CategoryStatus.DELETED));
-
-        Page<CategoryEntity> categoryEntityPage = new PageImpl<>(categoryEntities);
-
-        CategoryListCommand givenCategoryListCommand = CategoryListCommand.builder()
-                .pagination(Pagination.builder().pageNumber(1).pageSize(5).build())
-                .sorting(Sorting.builder().direction(Sort.Direction.ASC).property("name").build())
-                .filter(CategoryListCommand.CategoryFilter.builder()
-                        .name("Category")
-                        .statuses(Collections.singleton(CategoryStatus.ACTIVE)).build())
+        CategoryListCommand.CategoryFilter mockCategoryFilter = CategoryListCommand.CategoryFilter.builder()
+                .name("Category")
                 .build();
+
+        CategoryListCommand mockGivenCategoryListCommand = CategoryListCommand.builder()
+                .pagination(
+                        PaginationBuilder.builder()
+                                .pageNumber(1)
+                                .pageSize(2)
+                                .build()
+                )
+                .sorting(
+                        SortingBuilder.builder()
+                                .asc()
+                                .property("name")
+                                .build()
+                )
+                .filter(mockCategoryFilter)
+                .build();
+
+        // When
+        List<CategoryEntity> categoryEntities = new ArrayList<>();
+        categoryEntities.add(new CategoryEntity(
+                1L,
+                "Category 1",
+                CategoryStatus.ACTIVE)
+        );
+        categoryEntities.add(new CategoryEntity(
+                2L,
+                "Category 2",
+                CategoryStatus.DELETED)
+        );
+        Page<CategoryEntity> categoryEntityPage = new PageImpl<>(categoryEntities);
 
         Mockito.when(categoryRepository.findAll(
                 Mockito.<Specification<CategoryEntity>>any(),
                 Mockito.any(Pageable.class))
         ).thenReturn(categoryEntityPage);
 
-        RmaPage<Category> result = categoryService.getAllCategories(givenCategoryListCommand);
+        // Then
+        RmaPage<Category> result = categoryService.getAllCategories(mockGivenCategoryListCommand);
 
         // Assertions
         Mockito.verify(categoryRepository, Mockito.never()).findAll();
 
         Assertions.assertEquals(result.getContent().get(0).getName(), categoryEntities.get(0).getName());
         Assertions.assertEquals(result.getContent().get(1).getName(), categoryEntities.get(1).getName());
-        Assertions.assertEquals(result.getContent().get(2).getName(), categoryEntities.get(2).getName());
 
-        Assertions.assertEquals(3, result.getPageSize());
-        Assertions.assertEquals(result.getTotalElementCount(), categoryEntities.size());
-        Assertions.assertEquals(categoryEntities.size(), result.getContent().size());
+        Assertions.assertEquals(2, result.getPageSize());
+        Assertions.assertEquals(result.getTotalElementCount(),
+                mockGivenCategoryListCommand.getPagination().getPageSize()
+        );
+        Assertions.assertEquals(result.getFilteredBy(), mockGivenCategoryListCommand.getFilter());
     }
 
     @Test
     void givenGetAllCategories_whenResultIsNull_thenThrowNullPointerException() {
         // When
-
         CategoryListCommand categoryListCommand = CategoryListCommand.builder().build();
 
         // Then
