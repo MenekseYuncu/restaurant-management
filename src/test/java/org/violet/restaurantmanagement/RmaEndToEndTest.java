0b73af9a5bf10cba3d1373591c154240836fde7b
MenekseYuncu <baioretto66@gmail.com>
2024-05-22 15:05:43 +0300

    Unnecessary annotations deleted

--- src/test/java/org/violet/restaurantmanagement/RmaEndToEndTest.java
+++ src/test/java/org/violet/restaurantmanagement/RmaEndToEndTest.java
@@ -5,10 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class RmaEndToEndTest {
