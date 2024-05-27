5b622a0364272418b7b7bea4d518693bda225d14
MenekseYuncu <baioretto66@gmail.com>
2024-05-27 23:37:44 +0300

    modified the RmaController

--- src/test/java/org/violet/restaurantmanagement/RmaControllerTest.java
+++ src/test/java/org/violet/restaurantmanagement/RmaControllerTest.java
@@ -1,12 +1,8 @@
 package org.violet.restaurantmanagement;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 public abstract class RmaControllerTest {
 
     @Autowired
