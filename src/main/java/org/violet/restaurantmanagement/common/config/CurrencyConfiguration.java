620611f3cbe2c3a03c01c849e7dcce3198479bf9
MenekseYuncu <baioretto66@gmail.com>
2024-07-04 13:16:50 +0300

    currency enum added

--- src/main/java/org/violet/restaurantmanagement/common/config/CurrencyConfiguration.java
+++ src/main/java/org/violet/restaurantmanagement/common/config/CurrencyConfiguration.java
@@ -2,6 +2,7 @@ package org.violet.restaurantmanagement.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.violet.restaurantmanagement.common.model.enums.RmaCurrency;
 import org.violet.restaurantmanagement.parameter.model.ParameterEntity;
 import org.violet.restaurantmanagement.parameter.repository.ParameterRepository;
 
@@ -12,7 +13,7 @@ public class CurrencyConfiguration {
 
     @Bean
     String currency(ParameterRepository parameterRepository) {
-        Optional<ParameterEntity> parameterEntity = parameterRepository.findByName("Currency");
+        Optional<ParameterEntity> parameterEntity = parameterRepository.findByName(String.valueOf(RmaCurrency.CURRENCY));
         return parameterEntity.orElseThrow().getDefinition();
     }
 
