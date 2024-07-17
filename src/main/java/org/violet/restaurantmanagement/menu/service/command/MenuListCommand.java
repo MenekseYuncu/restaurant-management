a158e636f2439d872a055317b549f65cfac22331
MenekseYuncu <baioretto66@gmail.com>
2024-07-17 12:37:09 +0300

    Listing active products of active categories

--- src/main/java/org/violet/restaurantmanagement/menu/service/command/MenuListCommand.java
+++ src/main/java/org/violet/restaurantmanagement/menu/service/command/MenuListCommand.java
@@ -6,9 +6,11 @@ import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
+import org.violet.restaurantmanagement.category.model.enums.CategoryStatus;
 import org.violet.restaurantmanagement.common.model.Filtering;
 import org.violet.restaurantmanagement.common.model.mapper.RmaSpecification;
 import org.violet.restaurantmanagement.common.service.command.RmaPaginationCommand;
+import org.violet.restaurantmanagement.product.model.enums.ProductStatus;
 
 @Getter
 @SuperBuilder
@@ -28,11 +30,16 @@ public class MenuListCommand extends RmaPaginationCommand implements RmaSpecific
     @Override
     @SuppressWarnings("This method is unused by the application directly but Spring is using it in the background.")
     public <C> Specification<C> toSpecification(Class<C> clazz) {
-        if (this.filter == null) {
-            return Specification.allOf();
-        }
 
         Specification<C> specification = Specification.where(null);
+
+        Specification<C> statusProductSpecification = (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("status"), ProductStatus.ACTIVE);
+
+        Specification<C> statusCategorySpecification = (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.join("category").get("status"), CategoryStatus.ACTIVE);
+
+        specification = specification.and(statusProductSpecification).and(statusCategorySpecification);
 
         if (StringUtils.hasText(this.filter.getName())) {
 
