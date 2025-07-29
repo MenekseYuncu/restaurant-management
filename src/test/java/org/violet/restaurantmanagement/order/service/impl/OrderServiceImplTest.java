6d6dc7baf481c2a2da24400fa45be021f52f5581
menekseyuncu <menekseyuncu@gmail.com>
2025-07-29 19:53:24 +0300

    Remove unused imports and redundant test in OrderServiceImplTest

--- src/test/java/org/violet/restaurantmanagement/order/service/impl/OrderServiceImplTest.java
+++ src/test/java/org/violet/restaurantmanagement/order/service/impl/OrderServiceImplTest.java
@@ -1,16 +1,10 @@
 package org.violet.restaurantmanagement.order.service.impl;
 
-import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.violet.restaurantmanagement.RmaServiceTest;
 import org.violet.restaurantmanagement.RmaTestContainer;
 import org.violet.restaurantmanagement.order.repository.OrderRepository;
-
-import java.util.Optional;
-import java.util.UUID;
 
 class OrderServiceImplTest extends RmaServiceTest implements RmaTestContainer {
 
@@ -19,21 +13,6 @@ class OrderServiceImplTest extends RmaServiceTest implements RmaTestContainer {
 
     @InjectMocks
     private OrderServiceImpl orderService;
-
-
-    @Test
-    void givenInvalidMergeId_whenPlacingAnOrder_thenThrowException() {
-        // Given
-        String mergeId = UUID.randomUUID().toString();
-
-        // When
-        Mockito.when(orderRepository.findByMergeId(mergeId))
-                .thenReturn(Optional.empty());
-
-        // Verify
-        Mockito.verify(orderRepository, Mockito.never())
-                .save(ArgumentMatchers.any());
-    }
 
 
 }
