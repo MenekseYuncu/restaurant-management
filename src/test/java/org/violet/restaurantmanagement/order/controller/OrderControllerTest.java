dc9a521623a6d4d3cbe8f41ae86e202f6f6a3eee
menekseyuncu <menekseyuncu@gmail.com>
2025-08-15 21:59:17 +0300

    Add controller tests for removing items from orders

--- src/test/java/org/violet/restaurantmanagement/order/controller/OrderControllerTest.java
+++ src/test/java/org/violet/restaurantmanagement/order/controller/OrderControllerTest.java
@@ -2,6 +2,7 @@ package org.violet.restaurantmanagement.order.controller;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.dockerjava.api.exception.BadRequestException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -22,6 +23,7 @@ import org.violet.restaurantmanagement.order.controller.mapper.OrderDomainToOrde
 import org.violet.restaurantmanagement.order.controller.mapper.OrderRemoveItemRequestToCommandMapper;
 import org.violet.restaurantmanagement.order.controller.mapper.OrderUpdateRequestToCommandMapper;
 import org.violet.restaurantmanagement.order.controller.request.OrderCreateRequest;
+import org.violet.restaurantmanagement.order.controller.request.OrderRemoveItemRequest;
 import org.violet.restaurantmanagement.order.controller.request.OrderUpdateRequest;
 import org.violet.restaurantmanagement.order.controller.response.OrderResponse;
 import org.violet.restaurantmanagement.order.exceptions.InvalidItemQuantityException;
@@ -30,6 +32,7 @@ import org.violet.restaurantmanagement.order.model.OrderItemStatus;
 import org.violet.restaurantmanagement.order.model.OrderStatus;
 import org.violet.restaurantmanagement.order.service.OrderService;
 import org.violet.restaurantmanagement.order.service.command.OrderCreateCommand;
+import org.violet.restaurantmanagement.order.service.command.OrderRemoveItemCommand;
 import org.violet.restaurantmanagement.order.service.command.OrderUpdateCommand;
 import org.violet.restaurantmanagement.order.service.domain.Order;
 import org.violet.restaurantmanagement.order.service.domain.OrderItem;
@@ -459,6 +462,203 @@ class OrderControllerTest extends RmaControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.response").value("Validation failed"));
     }
 
+
+    @Test
+    void givenValidOrderRemoveItemRequest_whenRemoveItemProductFromOrder_thenUpdateQuantity() throws Exception {
+        // Given
+        String orderId = UUID.randomUUID().toString();
+        ProductEntity productEntity1 = ProductEntity.builder()
+                .id(UUID.randomUUID().toString())
+                .price(BigDecimal.valueOf(100))
+                .name("Product 1")
+                .build();
+        ProductEntity productEntity2 = ProductEntity.builder()
+                .id(UUID.randomUUID().toString())
+                .price(BigDecimal.valueOf(150))
+                .name("Product 2")
+                .build();
+
+        // When
+        OrderRemoveItemRequest.ProductItem productItem = new OrderRemoveItemRequest.ProductItem(productEntity1.getId(), 1);
+        OrderRemoveItemRequest removeItemRequest = new OrderRemoveItemRequest(List.of(productItem));
+
+        OrderRemoveItemCommand.ProductItem productItems = new OrderRemoveItemCommand.ProductItem(productEntity1.getId(), 1);
+        OrderRemoveItemCommand command = new OrderRemoveItemCommand(List.of(productItems));
+
+        Mockito.when(orderRemoveItemRequestToCommandMapper.map(any(OrderRemoveItemRequest.class)))
+                .thenReturn(command);
+
+        List<OrderItem> orderItems = List.of(
+                OrderItem.builder()
+                        .productId(productEntity1.getId()).quantity(2)
+                        .price(BigDecimal.valueOf(100))
+                        .status(OrderItemStatus.PREPARING).build(),
+                OrderItem.builder()
+                        .productId(productEntity2.getId()).quantity(1)
+                        .price(BigDecimal.valueOf(150))
+                        .status(OrderItemStatus.PREPARING).build()
+        );
+
+        Order mockOrder = new Order(
+                UUID.randomUUID().toString(),
+                UUID.randomUUID().toString(),
+                OrderStatus.OPEN,
+                BigDecimal.valueOf(350),
+                orderItems,
+                LocalDateTime.now(),
+                null
+        );
+
+        // When
+        Mockito.when(orderService.removeItemProductsFromOrder(ArgumentMatchers.eq(orderId), ArgumentMatchers.any(OrderRemoveItemCommand.class)))
+                .thenReturn(mockOrder);
+
+        OrderResponse mockOrderResponse = new OrderResponse(
+                mockOrder.getId(),
+                mockOrder.getStatus(),
+                List.of(
+                        new OrderResponse.OrderProductResponse(productEntity1.getId(), productEntity1.getName(), 2, productEntity1.getPrice()),
+                        new OrderResponse.OrderProductResponse(productEntity2.getId(), productEntity2.getName(), 1, productEntity2.getPrice())
+                ),
+                mockOrder.getTotalAmount(),
+                mockOrder.getCreatedAt(),
+                mockOrder.getUpdatedAt()
+        );
+
+        Mockito.when(orderDomainToOrderResponseMapper.map(any(Order.class)))
+                .thenReturn(mockOrderResponse);
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders.put(BASE_URL + "/{id}/remove", orderId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(removeItemRequest)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.isSuccess").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.orderId").value(mockOrder.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.products.length()").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].productId").value(productEntity1.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].price").value(100.0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.totalAmount").value(350.0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.updatedAt").isEmpty());
+
+        Mockito.verify(orderService, Mockito.times(1))
+                .removeItemProductsFromOrder(ArgumentMatchers.eq(orderId), ArgumentMatchers.any(OrderRemoveItemCommand.class));
+
+        Mockito.verify(orderRemoveItemRequestToCommandMapper, Mockito.times(1))
+                .map(any(OrderRemoveItemRequest.class));
+
+        Mockito.verify(orderDomainToOrderResponseMapper, Mockito.times(1))
+                .map(any(Order.class));
+    }
+
+    @Test
+    void givenInvalidProductId_whenRemoveItemProductFromOrder_thenNotFound() throws Exception {
+        // Given
+        String orderId = UUID.randomUUID().toString();
+        String invalidProductId = "invalid-product-id";
+
+        List<OrderRemoveItemRequest.ProductItem> productItems = List.of(
+                new OrderRemoveItemRequest.ProductItem(invalidProductId, 2)
+        );
+
+        OrderRemoveItemRequest mockOrderRequest = new OrderRemoveItemRequest(productItems);
+
+        OrderRemoveItemCommand removeItemCommand = new OrderRemoveItemCommand(
+                List.of(new OrderRemoveItemCommand.ProductItem(invalidProductId, 2))
+        );
+
+        // When
+        Mockito.when(orderRemoveItemRequestToCommandMapper.map(any(OrderRemoveItemRequest.class)))
+                .thenReturn(removeItemCommand);
+
+        Mockito.when(orderService.removeItemProductsFromOrder(orderId, removeItemCommand))
+                .thenThrow(new ProductNotFoundException());
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders.put(BASE_URL + "/{id}/remove", orderId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(mockOrderRequest)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.isSuccess").value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.httpStatus").value("NOT_FOUND"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response").value("Product does not exist"));
+
+        // Verify
+        Mockito.verify(orderService, Mockito.times(1))
+                .removeItemProductsFromOrder(ArgumentMatchers.eq(orderId), ArgumentMatchers.any(OrderRemoveItemCommand.class));
+
+        Mockito.verify(orderRemoveItemRequestToCommandMapper, Mockito.times(1))
+                .map(any(OrderRemoveItemRequest.class));
+    }
+
+    @Test
+    void givenInvalidQuantity_whenRemoveItemProductFromOrder_thenBadRequest() throws Exception {
+        // Given
+        String orderId = UUID.randomUUID().toString();
+        String productId = UUID.randomUUID().toString();
+
+        List<OrderRemoveItemRequest.ProductItem> productItems = List.of(
+                new OrderRemoveItemRequest.ProductItem(productId, -1)
+        );
+
+        OrderRemoveItemRequest mockOrderRequest = new OrderRemoveItemRequest(productItems);
+
+        OrderRemoveItemCommand removeItemCommand = new OrderRemoveItemCommand(
+                List.of(new OrderRemoveItemCommand.ProductItem(productId, -1))
+        );
+
+        // When
+        Mockito.when(orderRemoveItemRequestToCommandMapper.map(any(OrderRemoveItemRequest.class)))
+                .thenReturn(removeItemCommand);
+
+        Mockito.when(orderService.removeItemProductsFromOrder(orderId, removeItemCommand))
+                .thenThrow(new BadRequestException("Validation failed"));
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders.put(BASE_URL + "/{id}/remove", orderId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(mockOrderRequest)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.isSuccess").value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.httpStatus").value("BAD_REQUEST"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response").value("Validation failed"));
+    }
+
+    @Test
+    void givenMissingProductId_whenRemoveItemProductFromOrder_thenBadRequest() throws Exception {
+        // Given
+        String orderId = UUID.randomUUID().toString();
+
+        List<OrderRemoveItemRequest.ProductItem> productItems = List.of(
+                new OrderRemoveItemRequest.ProductItem("", 2)
+        );
+
+        OrderRemoveItemRequest mockOrderRequest = new OrderRemoveItemRequest(productItems);
+
+        OrderRemoveItemCommand removeItemCommand = new OrderRemoveItemCommand(
+                List.of(new OrderRemoveItemCommand.ProductItem("", 2))
+        );
+
+        // When
+        Mockito.when(orderRemoveItemRequestToCommandMapper.map(any(OrderRemoveItemRequest.class)))
+                .thenReturn(removeItemCommand);
+
+        Mockito.when(orderService.removeItemProductsFromOrder(orderId, removeItemCommand))
+                .thenThrow(new BadRequestException("Validation failed"));
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders.put(BASE_URL + "/{id}/remove", orderId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(mockOrderRequest)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.isSuccess").value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.httpStatus").value("BAD_REQUEST"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response").value("Validation failed"));
+    }
 
     @Test
     void givenValidCancelOrderId_whenOrderFound_thenReturnSuccess() throws Exception {
